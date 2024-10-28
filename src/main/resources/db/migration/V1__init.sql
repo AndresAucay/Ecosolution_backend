74a3ad6c7dd7ed2ef226d7bd909fc20778dafecc
MateoGalan666 <117777169+MateoGalan616@users.noreply.github.com>
2024-10-28 09:06:59 -0500

    prueba

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS table1 (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL,
-    description VARCHAR(100) NOT NULL,
+    hola VARCHAR(100) NOT NULL,
     description2 VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
     );
