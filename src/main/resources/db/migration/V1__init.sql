1bd6ff72ad0514befb7e22ad3ec489da791f5fad
MateoGalan666 <117777169+MateoGalan616@users.noreply.github.com>
2024-10-28 20:19:33 -0500

    mensaje

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,29 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL,
-    hola VARCHAR(100) NOT NULL,
-    description2 VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    codeText VARCHAR(100) NOT NULL,
+    fullname VARCHAR(50) NOT NULL,
+    cedula INT NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    roll_id INT,
+    password VARCHAR(25)
     );
 
+CREATE TABLE IF NOT EXISTS dispositivos (
+    id SERIAL PRIMARY KEY,
+    codeText VARCHAR(100),
+    location VARCHAR(100),
+    dateCreacion DATE,
+    estado VARCHAR(50),
+    last_mantenimiento DATE
+    );
+
+CREATE TABLE IF NOT EXISTS containers (
+    id SERIAL PRIMARY KEY,
+    codeText VARCHAR(100),
+    location VARCHAR(100),
+    capacidad INT,
+    last_emptying DATE,
+    filling_weight INT,
+    id_dispositivo INT,
+    FOREIGN KEY (id_dispositivo) REFERENCES dispositivos(id)
+    );
